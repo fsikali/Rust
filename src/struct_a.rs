3877b715b4fd1e2e3755fa5b337568861e2b34b6
fsikali <flemmingsikali499@gmail.com>
2024-06-14 23:27:52 +0300

    Modified: src/struct_a.rs

--- src/struct_a.rs
+++ src/struct_a.rs
@@ -5,20 +5,23 @@ struct Rectangle {
 } 
 
 pub fn get_area() { 
-    // Created a particular instance of the rectangle whose type is an immutable 
-    // borrow of a struct Rectangle instance 
+    // Created a particular instance of the Rectangle that has a 
+    // length of 30 and width of 10
 
     let rect1 = Rectangle { 
         length: 30, 
         width: 10,
     }; 
-
+    
+    // The rect_area function accesses the length and width fields of the Rectangle instance
     println!("This is the area: {}", rect_area(&rect1) );
    
 } 
  
-fn rect_area(rectangle: &Rectangle) -> u64 {  // The rect_area function accesses the length and the width
-      rectangle.length * rectangle.width      // fields of the Rectangle instance
+ // The rect_area function is defined with one parameter -> rectangle whose type
+ // is an immutable borrow of a struct Rectangle instance
+fn rect_area(rectangle: &Rectangle) -> u64 { 
+      rectangle.length * rectangle.width     
 }
 
 
