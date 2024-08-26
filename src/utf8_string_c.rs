0542bf3612975b48974b1cf8bfb4299d075afe44
fsikali <flemmingsikali499@gmail.com>
2024-08-26 23:49:31 +0300

    modified: src/utf8_string_c.rs

--- src/utf8_string_c.rs
+++ src/utf8_string_c.rs
@@ -7,3 +7,6 @@ pub fn my_string() {
 // Because strings are used for so many things, we can use many different generic 
 // APIs for stings, providing us with a lot of options 
 
+// N/B - Strings are UTF-8 encoded, so we include any properly encoded data in them 
+ 
+ 
