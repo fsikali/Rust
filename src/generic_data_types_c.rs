72e10a19f4c275976111c0c340036a4b4f451a58
fsikali <flemmingsikali499@gmail.com>
2024-09-30 23:36:46 +0300

    modified: src/generic_data_types_c.rs

--- src/generic_data_types_c.rs
+++ src/generic_data_types_c.rs
@@ -12,4 +12,6 @@ pub fn my_data_type() {
     let both_integer = Point{ x: 5, y: 10}; 
     let both_float = Point { x: 1.0, y: 4.0}; 
     let integer_and_float = Point { x: 5, y: 4.0}; 
-}
+}  
+
+
